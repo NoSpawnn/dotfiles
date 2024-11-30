c920420fe792a4998506eef79560f195c0d110fe
NoSpawnn <78027161+NoSpawnn@users.noreply.github.com>
2024-11-30 17:04:04 +0000

    Update scripts

---
+++ scripts/install_autosuggestions.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/zsh-users/zsh-autosuggestions ../zsh-autosuggestions
+source ../zsh-autosuggestions/zsh-autosuggestions.zsh
+
---
+++ scripts/install_ohmyposh.sh
@@ -0,0 +1,3 @@
+mkdir -p ~/.local/bin
+curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
+
