8e36002269a16d6956861cd3dcfaae93d2de01a7
Red <78027161+NoSpawnn@users.noreply.github.com>
2025-09-18 00:25:03 +0100

    waybar: add bluetooth control window rewrite

--- install.sh
+++ install.sh
@@ -2,6 +2,14 @@
 
 set -euxo pipefail
 
+ensure_flatpaks() {
+    local flatpaks=$(cat $(realpath $1) | tr '\n' ' ' )
+}
+
+ensure_flatpaks ./flatpaks.list
+
+exit 0
+
 # Install oh-my-posh
 curl -s https://ohmyposh.dev/install.sh | bash -s
 
