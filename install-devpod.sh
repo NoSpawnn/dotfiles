51979a2a08a06b607153a5fd6f4c1ee4bb969f45
Red <78027161+NoSpawnn@users.noreply.github.com>
2025-09-19 01:07:04 +0100

    dev: fix devpod install script to work with new file structure

--- install-devpod.sh
+++ install-devpod.sh
@@ -2,7 +2,9 @@
 
 # Install script for devpods/devcontainers
 
-cp ~/dotfiles/.zshrc ~/.zshrc
+SHELL_CONF_DIR="$HOME/dotfiles/shell"
+
+cp "$SHELL_CONF_DIR/.zshrc ~/.zshrc"
 
 curl -s https://ohmyposh.dev/install.sh | bash -s
-cp -r ~/dotfiles/.config/ohmyposh ~/.config/ohmyposh
+cp -r "$SHELL_CONF_DIR/.config/ohmyposh ~/.config/ohmyposh"
