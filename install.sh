f482c782824613f171ed6e1905c07f8d714573a4
Red <78027161+NoSpawnn@users.noreply.github.com>
2025-10-04 00:57:38 +0100

    scripts: remove distrobox from install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 
 set -euxo pipefail
 
-# Install oh-my-posh
+# Shell
+
+## Install oh-my-posh
 curl -s https://ohmyposh.dev/install.sh | bash -s
 
 # Symlink dotfiles
@@ -10,5 +12,3 @@ stow shell
 stow hyprland
 # Explicitly create the zed config dir, else stow will symlink every folder
 mkdir -p $HOME/.var/app/dev.zed.Zed/config/zed && stow zed
-
-distrobox assemble create -R --file distrobox.ini
