c58e1b591b1c5d5541c32c31f51d49eb2105f749
Red <78027161+NoSpawnn@users.noreply.github.com>
2026-04-13 11:49:29 +0100

    make luals recognize vim global

--- dots/dot_config/nvim/lua/config/lsp/luals.lua
+++ dots/dot_config/nvim/lua/config/lsp/luals.lua
@@ -2,5 +2,10 @@ vim.lsp.config("luals", {
 	cmd = { "lua-language-server" },
 	filetypes = { "lua" },
 	root_markers = { ".luarc.json", ".luarc.jsonc" },
+	settings = {
+		Lua = {
+			diagnostics = { globals = { "vim" } },
+		},
+	},
 })
 vim.lsp.enable("luals")
