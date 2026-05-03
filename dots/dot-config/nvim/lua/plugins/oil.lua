21597cd3ca79cff96b0082f5af02af13a9c3714c
Red <78027161+NoSpawnn@users.noreply.github.com>
2026-05-03 21:52:37 +0100

    map oil to key

--- dots/dot-config/nvim/lua/plugins/oil.lua
+++ dots/dot-config/nvim/lua/plugins/oil.lua
@@ -3,7 +3,9 @@
 return {
 	"stevearc/oil.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	lazy = false,
+    keys = {
+        { "<leader>fo", "<cmd>Oil<CR>", desc = "Oil" }
+    },
 	opts = {
 		columns = {
 			"icon",
