7f15f97df2424cd1dea7273375823888c806717c
Red <78027161+NoSpawnn@users.noreply.github.com>
2026-05-03 15:12:05 +0100

    overhaul nvim lsp

---
+++ dots/dot-config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -0,0 +1,21 @@
+return {
+    "neovim/nvim-lspconfig",
+    config = function()
+        local to_enable = {
+            "clangd",
+            "lua_ls",
+            "rst_analyzer",
+            "nil_ls"
+        }
+
+        local configs_path = vim.fn.stdpath("data") .. "/lazy/nvim-lspconfig/lsp"
+        for _, ls in ipairs(to_enable) do
+            local path = configs_path .. "/" .. ls .. ".lua"
+            if not vim.loop.fs_stat(path) then
+                vim.notify(("nvim-lspconfig: bad server: '%s'. see %s for valid servers"):format(ls, configs_path), vim.log.levels.WARN)
+            end
+        end
+
+        vim.lsp.enable(to_enable)
+    end
+}
