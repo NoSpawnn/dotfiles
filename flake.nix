b5a7c26ec7d20b3e0fbf6cc76415b6a2c758200d
Red <78027161+NoSpawnn@users.noreply.github.com>
2026-05-03 12:04:51 +0100

    add stow

--- flake.nix
+++ flake.nix
@@ -29,6 +29,7 @@
           default = pkgs.mkShellNoCC {
             packages = with pkgs; [
               shellcheck
+              stow
               self.formatter.${system}
             ];
           };
