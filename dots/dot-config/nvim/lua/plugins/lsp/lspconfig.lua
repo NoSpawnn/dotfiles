1c2de18cffa19b3a9e4d260801c8a03380462df1
Red <78027161+NoSpawnn@users.noreply.github.com>
2026-05-03 17:53:01 +0100

    config lsps

--- dots/dot-config/nvim/lua/plugins/lsp/lspconfig.lua
+++ dots/dot-config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -1,22 +1,40 @@
+local configs = {
+    clangd = {},
+    lua_ls = {},
+    rust_analyzer = {},
+
+    -- nil is better for everything except completion
+    nil_ls = {
+        capabilities = { completionProvider = false }
+    },
+    nixd = {
+        capabilities = {
+            codeActionProvider = nil,
+            definitionProvider = false,
+            documentFormattingProvider = false,
+            documentSymbolProvider = false,
+            documentHighlightProvider = false,
+            hoverProvider = false,
+            inlayHintProvider = false,
+            referencesProvider = false,
+            renameProvider = false
+        }
+    }
+}
+
 return {
     "neovim/nvim-lspconfig",
     config = function()
-        local to_enable = {
-            "clangd",
-            "lua_ls",
-            "rust_analyzer",
-            "nil_ls"
-        }
-
         local configs_path = vim.fn.stdpath("data") .. "/lazy/nvim-lspconfig/lsp"
-        for _, ls in ipairs(to_enable) do
+        for ls, config in pairs(configs) do
             local path = configs_path .. "/" .. ls .. ".lua"
-            if not vim.loop.fs_stat(path) then
-                vim.notify(("nvim-lspconfig: bad server: '%s'. see %s for valid servers"):format(ls, configs_path),
-                    vim.log.levels.WARN)
+            if vim.loop.fs_stat(path) then
+                vim.lsp.config(ls, config)
+                vim.lsp.enable(ls)
+            else
+                local msg = ("nvim-lspconfig: bad server: '%s'. see %s for valid servers"):format(ls, configs_path)
+                vim.notify(msg, vim.log.levels.WARN)
             end
         end
-
-        vim.lsp.enable(to_enable)
     end
 }
