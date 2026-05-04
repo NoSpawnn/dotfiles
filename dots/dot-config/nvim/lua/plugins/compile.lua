7515bbb58f7334148efc45456772ad6428ee831f
Red <78027161+NoSpawnn@users.noreply.github.com>
2026-05-04 17:27:35 +0100

    add baleia as dep of compile-mode

--- dots/dot-config/nvim/lua/plugins/compile.lua
+++ dots/dot-config/nvim/lua/plugins/compile.lua
@@ -1,24 +1,30 @@
 -- https://github.com/ej-shafran/compile-mode.nvim
 
 return {
-	"ej-shafran/compile-mode.nvim",
-	version = "*",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		vim.g.compile_mode = {
-			bang_expansion = true,
-			focus_compilation_buffer = false,
-			default_command = {
-				rust = "cargo run",
-			},
-			error_regexp_table = {
-				rustc = {
-					regex = [[^\s*-->\s*\([^:]\+\):\(\d\+\):\(\d\+\)]],
-					filename = 1,
-					row = 2,
-					col = 3,
-				},
-			},
-		}
-	end,
+    "ej-shafran/compile-mode.nvim",
+    version = "*",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        { "m00qek/baleia.nvim", version = false }
+    },
+    config = function()
+        vim.g.compile_mode = {
+            bang_expansion = true,
+            baleia_setup = true,
+            focus_compilation_buffer = true,
+
+            default_command = {
+                rust = "cargo run",
+            },
+
+            error_regexp_table = {
+                rustc = {
+                    regex = [[^\s*-->\s*\([^:]\+\):\(\d\+\):\(\d\+\)]],
+                    filename = 1,
+                    row = 2,
+                    col = 3,
+                },
+            },
+        }
+    end,
 }
