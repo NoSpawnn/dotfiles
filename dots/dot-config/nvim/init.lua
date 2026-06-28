186f6baac518eca6a9d9b112923625c485b2a8a6
Red <78027161+NoSpawnn@users.noreply.github.com>
2026-06-28 22:51:34 +0100

    more nvim config

--- dots/dot-config/nvim/init.lua
+++ dots/dot-config/nvim/init.lua
@@ -3,51 +3,60 @@ local vim = vim
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-vim.cmd.colorscheme("industry")
-
 vim.opt.relativenumber = true
+
+-- this is already in lualine
+vim.opt.showmode = false
 
 -- undo config
 local undodir = vim.fn.stdpath("data") .. "/undodir"
 if not vim.fn.isdirectory(undodir) then
     vim.fn.mkdir(undodir, "p")
 end
-vim.opt.undodir = undodir
-vim.opt.undofile = true
-vim.opt.swapfile = false
+vim.opt.undodir    = undodir
+vim.opt.undofile   = true
+vim.opt.swapfile   = false
 
 -- indent settings: see https://gist.github.com/LunarLambda/4c444238fb364509b72cfb891979f1dd, these are way more confusing than they should be
-vim.opt.expandtab = true
-vim.opt.smarttab = true
+vim.opt.expandtab  = true
+vim.opt.smarttab   = true
 vim.opt.shiftwidth = 4
 
 -- better search
-vim.opt.hlsearch       = true
-vim.opt.incsearch      = true
-vim.opt.ignorecase     = true
-vim.opt.smartcase      = true
+vim.opt.hlsearch   = true
+vim.opt.incsearch  = true
+vim.opt.ignorecase = true
+vim.opt.smartcase  = true
 vim.opt.wildoptions:append("fuzzy") -- better completion with fuzzy search
 
 -- prefer vsplits below
-vim.opt.splitbelow     = true
+vim.opt.splitbelow = true
+
+-- always show sign gutter col to avoid text jumping left and right
+vim.opt.signcolumn = "yes"
 
 -- display certain whitespace
-vim.opt.listchars = "trail:~,tab:>-"
-vim.opt.list = true
-
+vim.opt.listchars  = "trail:~,tab:>-"
+vim.opt.list       = true
 
 -- use system clipboard
-vim.opt.clipboard = "unnamedplus"
+vim.opt.clipboard  = "unnamedplus"
 
 -- clear last / search with escape
-vim.keymap.set("n", "<ESC>", "<CMD>nohlsearch | let @/ = ''<CR>", { silent = true, noremap = true })
+vim.keymap.set("n", "<ESC>", "<CMD>nohlsearch | let @/ = ''<CR>", { silent = true })
+
+-- quickfix list, i need to learn to use this properly
+vim.keymap.set("n", "<leader>co", "<CMD>:copen<CR>")
+vim.keymap.set("n", "<leader>cc", "<CMD>:cclose<CR>")
 
 -- buffer keybinds
-vim.keymap.set("n", "<leader>bd", ":bdelete<CR>", {})
+vim.keymap.set("n", "<leader>bd", ":bdelete<CR>")
+vim.keymap.set("n", "<leader>bn", ":bnext<CR>")
+vim.keymap.set("n", "<leader>bp", ":bprev<CR>")
 
--- alt-x for :
-vim.keymap.set("n", "<M-x>", ":", { noremap = true })
-vim.keymap.set("i", "<M-x>", "<ESC>:", { noremap = true })
+-- yoinking some stuff from emacs
+vim.keymap.set({ "n", "i" }, "<M-x>", "<ESC>:")
+vim.keymap.set({ "n", "i" }, "<C-x><C-s>", "<ESC>:w<CR>")
 
 -- emacs-like describe
 vim.api.nvim_create_user_command(
@@ -61,14 +70,14 @@ vim.api.nvim_create_user_command(
 )
 
 -- netrw
-vim.g.netrw_banner = 0       -- no banner
-vim.g.netrw_liststyle = 3    -- tree style
-vim.g.netrw_browse_split = 4 -- always vertical
-vim.g.netrw_keepdir = 0      -- change cwd as needed
-vim.g.netrw_preview = 1      -- display in vertical split
+vim.g.netrw_banner       = 0                           -- no banner
+vim.g.netrw_liststyle    = 3                           -- tree style
+vim.g.netrw_browse_split = 4                           -- always vertical
+vim.g.netrw_keepdir      = 0                           -- change cwd as needed
+vim.g.netrw_preview      = 1                           -- display in vertical split
 
-vim.keymap.set("n", "<leader>fee", "<CMD>Explore<CR>")  -- take over window
-vim.keymap.set("n", "<leader>fev", "<CMD>Lexplore<CR>") -- open in left split
+vim.keymap.set("n", "<leader>fe", "<CMD>Explore<CR>")  -- take over window
+vim.keymap.set("n", "<leader>fv", "<CMD>Lexplore<CR>") -- open in left split
 
 -- plugins
 vim.pack.add({
@@ -81,13 +90,16 @@ vim.pack.add({
     "github.com/nospawnn/align.nvim"
 })
 
-require("lualine").setup({ options = { theme = "16color" } })
-
+require("telescope").setup({
+    defaults = {
+        layout_strategy = "vertical"
+    },
+})
 local telescope_builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", telescope_builtin.find_files, { desc = "Telescope find files" })
-vim.keymap.set("n", "<leader>fg", telescope_builtin.live_grep,  { desc = "Telescope live grep" })
-vim.keymap.set("n", "<leader>fb", telescope_builtin.buffers,    { desc = "Telescope buffers" })
-vim.keymap.set("n", "<leader>fh", telescope_builtin.help_tags,  { desc = "Telescope help tags" })
+vim.keymap.set("n", "<leader>fg", telescope_builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", telescope_builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", telescope_builtin.help_tags, { desc = "Telescope help tags" })
 
 -- snippets/abbreviations
 for name, body in pairs({
@@ -103,18 +115,46 @@ for ft, abbrs in pairs({
         fmain = "int main() {<CR>}<ESC>Oreturn;<ESC>hi"
     }
 }) do
-    if next(abbrs) ~= nil then
-        for name, body in pairs(abbrs) do
-            vim.api.nvim_create_autocmd(
-                "FileType",
-                {
-                    pattern = ft,
-                    callback = function() vim.cmd.inoreabbrev("<buffer> " .. name, body) end
-                }
-            )
-        end
+    for name, body in pairs(abbrs) do
+        vim.api.nvim_create_autocmd(
+            "FileType",
+            {
+                pattern = ft,
+                callback = function() vim.cmd.inoreabbrev("<buffer> " .. name, body) end
+            }
+        )
     end
 end
 
--- a sprinkle of lsp
-vim.lsp.enable({ "lua_ls", "clangd" })
+-- lsp
+vim.lsp.enable({ "lua_ls", "clangd", "rust-analyzer" })
+vim.diagnostic.config({ virtual_text = true })
+vim.opt.completeopt:append("fuzzy,menuone,noselect,popup")
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('my.lsp', {}),
+    callback = function(ev)
+        local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+        if client:supports_method('textDocument/implementation') then
+            vim.keymap.set("i", "<C-space>", vim.lsp.completion.get)
+            vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename)
+            vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action)
+            vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float)
+
+            if client:supports_method('textDocument/formatting') then
+                vim.keymap.set(
+                    { "n", "x" },
+                    "<leader>lf",
+                    function() vim.lsp.buf.format({ bufnr = ev.buf, id = client.id }) end
+                )
+            end
+        end
+
+        if client:supports_method('textDocument/completion') then
+            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+        end
+    end,
+})
+
+-- theme
+vim.cmd.colorscheme("industry")
+require("lualine").setup({ options = { theme = "iceberg" } })
