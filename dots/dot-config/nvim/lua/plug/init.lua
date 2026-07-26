7690e5c7be3aea9356cfb76c288fabb245524173
Red <78027161+NoSpawnn@users.noreply.github.com>
2026-07-26 13:22:33 +0100

    wip auto import nvim lua files

--- dots/dot-config/nvim/lua/plug/init.lua
+++ dots/dot-config/nvim/lua/plug/init.lua
@@ -5,3 +5,10 @@ require("plug.oil")
 require("plug.telescope")
 require("plug.lsp")
 
+-- local handle = io.popen([[ find -name "*.lua" -exec basename {} \; | sort ]])
+-- local files = handle:read("a")
+-- handle:close()
+--
+-- for f in files:gsub("[\n]") do
+--     vim.notify(f)
+-- end
