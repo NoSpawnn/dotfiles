9c7cc5078e031fc3d0fc8ea14a4ed7f9491bc23d
Red <me@nospawnn.com>
2026-09-12 01:25:38 +0100

    nvim: remove log

--- dots/dot-config/nvim/lua/plug/init.lua
+++ dots/dot-config/nvim/lua/plug/init.lua
@@ -4,9 +4,6 @@ local plug_dir = config_dir .. "/lua/plug"
 for path in vim.fn.glob(plug_dir .. "/**/*.lua"):gmatch("[^%s]+") do
     local relpath = path:sub(#plug_dir + 2)
     local module_name = relpath:gsub("%.lua$", ""):gsub("/", ".")
-
-    vim.notify("Found module: " .. module_name, vim.log.levels.DEBUG)
-
     if module_name ~= "init" then
         require("plug." .. module_name)
     end
