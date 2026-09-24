7682a2c5d70942c660d764a688e807c7b12679c4
Red <me@nospawnn.com>
2026-09-24 08:59:45 +0100

    nvim: customize gitsigns symbols

--- dots/dot-config/nvim/lua/plug/gitsigns.lua
+++ dots/dot-config/nvim/lua/plug/gitsigns.lua
@@ -2,3 +2,21 @@ vim.pack.add({
     "https://github.com/lewis6991/gitsigns.nvim",
 })
 
+require("gitsigns").setup({
+  signs = {
+    add          = { text = '+' },
+    change       = { text = '~' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked    = { text = '?' },
+  },
+  signs_staged = {
+    add          = { text = 'S+' },
+    change       = { text = 'S~' },
+    delete       = { text = 'S_' },
+    topdelete    = { text = 'S‾' },
+    changedelete = { text = 'S~' },
+    untracked    = { text = 'S?' },
+  },
+})
